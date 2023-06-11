7941313fe10a3072946937ce3f2f13b3ef6f7123
gerardo cerezo <geranetx86@gmail.com>
2023-06-11 18:44:19 -0300

    chat ws-hbs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
-import express from 'express';
+import express from 'express'
+import handlebars from 'express-handlebars'
+import { Server } from 'socket.io'
+import viewsRouter from './routers/views.router.js'
+
 const app = express()
+const httpServer = app.listen(8080, () => console.log('Srv Up!'))
+const io = new Server(httpServer)
 
-app.get('/', (req, res) => res.send('ok'))
+app.engine('handlebars', handlebars.engine())
+app.set('views', './src/views')
+app.set('view engine', 'handlebars')
+app.use(express.static('./src/public'))
+app.use('/', viewsRouter)
 
-app.listen(8080, () => console.log('Server up!'))
+const messages = []
+
+io.on('connection', socket => {
+    socket.broadcast.emit('alerta')
+    socket.emit('logs', messages)
+    socket.on('message', data => {
+        messages.push(data)
+        io.emit('logs', messages)
+    })
+})
+
